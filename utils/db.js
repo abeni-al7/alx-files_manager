5e8ff7f2347a45e58962e23a2333bb28e3140716
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-31 21:26:41 +0300

    Create a class for mongoDB database operations

---
+++ utils/db.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.client = new MongoClient(`mongodb://${this.host}:${this.port}/${this.database}`, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const users = this.client.db(this.database).collection('users');
+    return users.countDocuments();
+  }
+
+  async nbFiles() {
+    const files = this.client.db(this.database).collection('files');
+    return files.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
