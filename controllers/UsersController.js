41b52301772f566177db7c3b936d733c8723716e
abeni-al7 <abenezeralebachew3@gmail.com>
2024-09-01 23:10:40 +0300

    Fix status code for response in POST users API

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,32 +1,34 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 const postNew = async (req, res) => {
   try {
     const db = dbClient.client.db(dbClient.database);
     const collection = db.collection('users');
     if (!req.body.email) {
-      return res.status(400).json({error: 'Missing email'});
-    } else if (!req.body.password) {
-      return res.status(400).json({error: 'Missing password'});
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!req.body.password) {
+      return res.status(400).json({ error: 'Missing password' });
     }
     const existingUser = await collection.findOne({ email: req.body.email });
     if (existingUser) {
-      return res.status(400).json({error: 'Already exist'});
+      return res.status(400).json({ error: 'Already exist' });
     }
     const result = await collection.insertOne({
       email: req.body.email,
       password: sha1(req.body.password),
     });
-    return res.json({
+    return res.status(201).json({
       id: result.insertedId,
       email: result.ops[0].email,
     });
-  } catch(err) {
+  } catch (err) {
     console.log(err);
+    return err;
   }
-}
+};
 
 export default {
   postNew,
-}
+};
