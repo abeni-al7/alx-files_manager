581ebd88a32985622856d33d1dfd3091616890da
abeni-al7 <abenezeralebachew3@gmail.com>
2024-09-03 03:50:02 +0300

    aFix

--- utils/redis.js
+++ utils/redis.js
@@ -27,9 +27,9 @@ class RedisClient {
   }
 
   async set(key, value, duration) {
-    const setAsync = util.promisify(this.client.set).bind(this.client);
+    const setAsync = util.promisify(this.client.setEx).bind(this.client);
     try {
-      await setAsync(key, value, 'EX', duration);
+      await setAsync(key, duration, value);
     } catch (err) {
       console.log(err);
     }
